eab08ed246bfeb31b14f0ba6b2af4bacfec38f14
Nisha Anandrao Kadam <142510987+NishaKadam05@users.noreply.github.com>
2024-07-23 18:04:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function clickMe(){
+    document.getElementsByClassName("val")[0].innerHTML='Background Color : Blue'
+    document.getElementsByClassName("background")[0].style.backgroundColor="blue";
+}
+
+function pink(){
+    document.getElementsByClassName("val")[0].innerHTML='Background Color : Pink'
+    document.getElementsByClassName("background")[0].style.backgroundColor="pink";
+}
+
+function green(){
+    document.getElementsByClassName("val")[0].innerHTML='Background Color : Green'
+    document.getElementsByClassName("background")[0].style.backgroundColor="green";
+}
+
+function purple(){
+    document.getElementsByClassName("val")[0].innerHTML='Background Color : Purple'
+    document.getElementsByClassName("background")[0].style.backgroundColor="purple";
+}
+
+function yellow(){
+    document.getElementsByClassName("val")[0].innerHTML='Background Color : Yellow'
+    document.getElementsByClassName("background")[0].style.backgroundColor="yellow";
+}
+    
